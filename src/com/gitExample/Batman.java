0da37e05b017036b7587eac16f55e51eba4d6a11
OrCohen <orc1535@gmail.com>
2020-08-03 18:52:43 +0300

    Update Batman.java

--- src/com/gitExample/Batman.java
+++ src/com/gitExample/Batman.java
@@ -1,9 +1,7 @@
 package com.gitExample;
 
-import java.lang.reflect.Array;
-
 public class Batman {
-    private int power = 6700;
+    private int powere = 6700;
     private String[] tools = {"Bat-mobile", "Cape", "Ninja stars"};
     private String color = "Black";
 
